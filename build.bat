f194777ae19dde0fc81ced3c539b182f7d9f2b11
www <a@a.com>
2022-05-22 21:58:21 +0200

    Opt out from .NET telemetry

--- build.bat
+++ build.bat
@@ -1,10 +1,12 @@
 @echo off
 
+set DOTNET_CLI_TELEMETRY_OPTOUT=1
+
 for /F "tokens=*" %%f in ('dir /B /AD /S bin') do rmdir /S /Q "%%f"
 for /F "tokens=*" %%f in ('dir /B /AD /S obj') do rmdir /S /Q "%%f"
-rmdir /S /Q Build
-rmdir /S /Q Debug
-rmdir /S /Q Release
+if exist Build rmdir /S /Q Build
+if exist Debug rmdir /S /Q Debug
+if exist Release rmdir /S /Q Release
 
 mkdir Build
 mkdir Build\VNTextPatch
