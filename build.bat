aab2228f37fde4ffc75b669646c81654746f3f24
www <a@a.com>
2022-05-22 22:07:33 +0200

    Allow unsafe code Explicitly set VNTextProxy platform

--- build.bat
+++ build.bat
@@ -13,14 +13,14 @@ mkdir Build\VNTextPatch
 mkdir Build\VNTextProxy
 
 dotnet restore VNTextPatch\VNTextPatch.csproj /p:RuntimeIdentifiers=win
-msbuild VNTextPatch\VNTextPatch.csproj /p:Configuration=Release /p:OutputPath=..\Build\VNTextPatch\ /p:LangVersion=9
+msbuild VNTextPatch\VNTextPatch.csproj /p:LangVersion=9 /AllowUnsafeBlocks=true /p:Configuration=Release /p:OutputPath=..\Build\VNTextPatch\
 del Build\VNTextPatch\FreeMote*.xml
 del Build\VNTextPatch\*.pdb
 del Build\VNTextPatch\*.txt
 
 for /D %%p in (VNTextProxy\AlternateProxies\*) do (
     copy /Y VNTextProxy\AlternateProxies\%%~np\*.* VNTextProxy
-    msbuild VNTextProxy\VNTextProxy.vcxproj /p:Configuration=Release /p:TargetName=%%~np
+    msbuild VNTextProxy\VNTextProxy.vcxproj /p:Platform=Win32 /p:Configuration=Release /p:TargetName=%%~np
     copy /Y VNTextProxy\Release\%%~np.dll Build\VNTextProxy
     rmdir /S /Q VNTextProxy\Release
 )
